0fe045c639755d50358b42fc123341f13f233993
swaner2 <86748529+swaner2@users.noreply.github.com>
2021-07-27 12:06:40 -0600

    Add files via upload

--- ProjectZero.py
+++ ProjectZero.py
@@ -1,3 +1,8 @@
+suit = {
+    "S": 1,
+    "H": 2,
+    "D": 3,
+    "C": 4}
 def card_coordinates(card):
     length = 1200
     width = 544
@@ -6,17 +11,12 @@ def card_coordinates(card):
         card_value = int(card[1])
     else:
         card_value = int(card[1] + card[2])
-    suit = {
-        "S": 1,
-        "H": 2,
-        "D": 3,
-        "C": 4}
     card_suit = suit[card_suit]
     x_coordinate = length // 13 * (card_value - 1)
     y_coordinate = width // 4 * (card_suit - 1)
     return (x_coordinate, y_coordinate)
 
-
+import random
 import pygame
 pygame.init()
 screen = pygame.display.set_mode(size=(800,600))
@@ -24,7 +24,6 @@ image = pygame.image.load("cards.png")
 
 number_of_players = int(input())
 def shuffle_deck():
-    import random
     cards_deck = []
     suits = ["C", "S", "H", "D"]
     for i in range(1, 14):
@@ -72,20 +71,49 @@ def game():
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONUP:
-
                 for i7 in range(len(all_player_decks[player_number])):
                     if pygame.mouse.get_pos()[0] > x_coordinates[i7] and pygame.mouse.get_pos()[0] < (x_coordinates[i7] + card_width) and pygame.mouse.get_pos()[1] > 464:
-                        if all_player_decks[player_number][i7][0] == middle_card[len(middle_card) - 1][0] or all_player_decks[player_number][i7][1] == middle_card[len(middle_card) - 1][1]:
-                            print(all_player_decks[player_number][i7])
+                        if all_player_decks[player_number][i7][0] == middle_card[len(middle_card) - 1][0] and all_player_decks[player_number][i7][1] == "8":
+                            print("spades, hearts, diamonds, or clubs?")
                             middle_card.append(all_player_decks[player_number][i7])
                             all_player_decks[player_number].pop(i7)
+                            choose_suit = input()
+                            if choose_suit == "spades":
+                                middle_card.append("S8")
+                            elif choose_suit == "hearts":
+                                middle_card.append("H8")
+                            elif choose_suit == "diamonds":
+                                middle_card.append("D8")
+                            elif choose_suit == "clubs":
+                                middle_card.append("C8")
                             player_number += 1
                             player_number %= number_of_players
+                        if len(middle_card[len(middle_card)-1]) == len(all_player_decks[player_number][i7]):
+                            if len(middle_card[len(middle_card) - 1]) < 3:
+                                print(all_player_decks[player_number][i7])
+                                if all_player_decks[player_number][i7][0] == middle_card[len(middle_card) - 1][0] or all_player_decks[player_number][i7][1] == middle_card[len(middle_card) - 1][1]:
+                                    middle_card.append(all_player_decks[player_number][i7])
+                                    all_player_decks[player_number].pop(i7)
+                                    player_number += 1
+                                    player_number %= number_of_players
+                            if len(middle_card[len(middle_card) - 1]) == 3:
+                                print(all_player_decks[player_number][i7])
+                                if all_player_decks[player_number][i7][0] == middle_card[len(middle_card) - 1][0] or (all_player_decks[player_number][i7][1] == middle_card[len(middle_card) - 1][1] and all_player_decks[player_number][i7][2] == middle_card[len(middle_card) - 1][2]):
+                                    middle_card.append(all_player_decks[player_number][i7])
+                                    all_player_decks[player_number].pop(i7)
+                                    player_number += 1
+                                    player_number %= number_of_players
+
                 if pygame.mouse.get_pos()[0] > 400 and pygame.mouse.get_pos()[0] < 400 + card_width and pygame.mouse.get_pos()[1] > 300 - card_length and pygame.mouse.get_pos()[1] < 300:
                     all_player_decks[player_number].append(shuffled[0])
                     shuffled.pop(0)
                     player_number += 1
                     player_number %= number_of_players
+                    if len(shuffled) == 0:
+                        for i6 in range(len(middle_card)-1):
+                            shuffled.append(middle_card[0])
+                            middle_card.pop(0)
+                            random.shuffle(shuffled)
                 print(pygame.mouse.get_pos())
         # drawing player decks
         x_coordinates = (x_center_player_deck(player_number))
@@ -102,3 +130,6 @@ def game():
         pygame.display.update()
 
 game()
+
+S13
+S1
